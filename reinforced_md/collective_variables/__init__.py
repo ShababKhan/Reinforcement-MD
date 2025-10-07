d13d0eaac2c11fb0367cf46ec77416537385ba12
Shabab Khan <shababmkhan@outlook.com>
2025-10-07 03:55:56 -0400

    Add collective variables module init

---
+++ reinforced_md/collective_variables/__init__.py
@@ -0,0 +1,6 @@
+"""
+Collective variables module for reinforced molecular dynamics.
+
+This module contains classes for defining and calculating collective variables (CVs)
+that capture biologically relevant protein motions.
+"""
