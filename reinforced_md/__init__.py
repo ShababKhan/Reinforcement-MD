8ec32cf29f0c8fccb00b9aa418685dee5b0db6d7
Shabab Khan <shababmkhan@outlook.com>
2025-10-07 03:55:27 -0400

    Add package init file

---
+++ reinforced_md/__init__.py
@@ -0,0 +1,9 @@
+"""
+Reinforced Molecular Dynamics (rMD) package.
+
+This package implements the reinforced molecular dynamics (rMD) approach described in the paper
+"Reinforced molecular dynamics: Physics-infused generative machine learning model explores CRBN 
+activation process" by István Kolossváry and Rory Coffey.
+"""
+
+__version__ = '0.1.0'
